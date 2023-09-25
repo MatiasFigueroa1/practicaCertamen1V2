8fee1b3494c24d0c3df0daee0db8e389b5f2f251
Matias Figueroa <matias.figueroa50@inacapmail.cl>
2023-09-25 00:16:48 -0300

    rama2 casi lista

---
+++ app2/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def vista2(request):
+    html="""
+<h1 style="color:red">Estás en la vista2</h1>
+"""
+    return  HttpResponse(html)
